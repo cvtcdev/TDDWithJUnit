5ff86e3503c476a48657ebe19f279a1765256c13
cvtcdev <edumholt@student.cvtc.edu>
2015-09-11 10:28:46 -0500

    Modified decelerate method on Car so as to not allow speed below 0.

--- src/Car.java
+++ src/Car.java
@@ -43,7 +43,11 @@ public class Car {
 	 * @param amount	The amount the car will decelerate
 	 */
 	public void decelerate(int amount){
-		this.speed-= amount;
+		if (this.speed - amount <= 0){
+			this.speed = 0;
+		} else {
+			this.speed-= amount;
+		}
 	}
 	
 	/**
